637d34530c2d9acb19374ad2035bde091a091e74
Saransh Mehta <101050311+saranshmehta33@users.noreply.github.com>
2022-08-24 23:09:46 +0530

    Time: 180 ms (26.52%), Space: 46.6 MB (92.05%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -7,14 +7,8 @@ public:
         for(int i=0;i<nums.size()-1;i++)
         {
             if(nums[i]==nums[i+1])
-            {
-                 flag=1;
-                break;
-            }
+                return true;
         }
-        if(flag)
-            return true;
-        else
-            return false;
+        return false;
     }
 };
