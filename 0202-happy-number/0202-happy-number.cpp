2d044b2646f938e2c85a39cac919f12e4f3396cd
Saransh Mehta <101050311+saranshmehta33@users.noreply.github.com>
2022-11-21 12:37:10 +0530

    Time: 5 ms (34.60%), Space: 6.4 MB (6.33%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -13,7 +13,7 @@ public:
             {
                 index = n%10;
                 val += index*index;
-                n /=10;
+                n =n/10;
             }
             if(val==1)
                 return true;
